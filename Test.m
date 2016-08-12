5a770a1a5819eb99d15df129c155c51472b8aa86
Ali Khodabakhsh <AliKhoda@users.noreply.github.com>
2016-08-12 17:35:44 +0430

    Correct typo

--- Test.m
+++ Test.m
@@ -5,7 +5,7 @@ clc;
 % Maximum number to check
 lim = 1000;
 
-% Check from 2 to lim, whether it gives same result as matlab'd defaul isprime
+% Check from 2 to lim, whether aks gives same result as MATLAB's default isprime function
 for k = 2 : lim
     fprintf('%d\t',k);
     tic;
